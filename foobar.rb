529ff5014abd48b7c0841153422bbf80fda16eb3
Yuting Wang <yttfwang@berkeley.edu>
2015-09-18 20:08:55 -0700

    finish hw 1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,13 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+    a.map! { |i| i.to_i }
+    a.map! { |d| d = d + 2}
+    b = a.select { |x| x % 2 == 0 }
+    b = b.uniq
+    b = b.select { |x| x <= 10}
+    sum = 0
+    b.each { |x| sum = sum + x}
+    sum
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,20 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  y = a + b;
+  y ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  sorted = a.sort;
+  sorted.map! { |d| d = d + 1}
+  sorted
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  name = first_name + " " + last_name
+  name
 end
 
 def blockin_time(a)
